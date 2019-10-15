be09fe171838290b6fab1a90dc727c58825daa4c
roysubs <roysubs@hotmail.com>
2019-10-16 01:54:39 +0200

    basic changes

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,13 @@
+function syn($cmd) {
+    Get-Command $cmd -Syntax
+}
+
+# git init
+# git add Microsoft.PowerShell_profile.ps1
+# git status
+# git commit -m "adding files"
+# git remote add origin https://github.com/roysubs/psprofile.git
+# git pull origin master --allow-unrelated-histories
+    # Had to do this to force the merge to happen.
+# git push -u origin master
+# git clone https://github.com/roysubs/psprofile.git
